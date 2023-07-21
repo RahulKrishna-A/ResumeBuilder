402c845011718f29c444e5dd92f8a4ebaff5c41a
Rahul <rahulkrish321123@gmail.com>
2023-07-22 03:12:10 +0530

    User experience --Raw

--- src/comps/Content.jsx
+++ src/comps/Content.jsx
@@ -1,21 +1,23 @@
 import {useState} from "react";
 import { v4 as uuidv4 } from 'uuid';
+import UserDetails from "./UserDetails.jsx";
 
 export default function Content(){
     const [UserDetailes,setUserDetails]=useState({
-        FirstName:null,
-        LasttName:null,
-        ContactNo:null,
-        Email:null,
-        Location:null,
-        Description:null,
+        FirstName:0,
+        LastName:0,
+        ContactNo:0,
+        Email:0,
+        Location:0,
+        Description:0,
     })
 
-    function onUserDetails(e){
+    function onChangeUserDetails(e){
         setUserDetails((prev)=>{
+            console.log(prev)
             return {
                 ...prev,
-                [e.target.Name]:e.target.value
+                [e.target.name]:e.target.value
             }
         })
     }
@@ -25,11 +27,11 @@ export default function Content(){
     function onAddExperience(e){
         let newExperience = {
             "id":uuidv4(),
-            "position": null,
-            "companyName":null,
-            "startDate":null,
-            "endDate":null,
-            "summary":null
+            "position": 0,
+            "companyName":0,
+            "startDate":0,
+            "endDate":0,
+            "summary":0
         }
         setProfessionalExperience((prev)=>{
             return [...prev,newExperience]
@@ -38,25 +40,29 @@ export default function Content(){
 
     const [Education,setEducation]=useState([]);
 
-    function oneducation(e){
+    function onAddeducation(e){
         let newEducation = {
             "id":uuidv4(),
-            "School Name": null,
-            "School Location":null,
-            "startDate":null,
-            "endDate":null,
-            "Field of Study":null,
-            "Description":null
+            "School Name": 0,
+            "School Location":0,
+            "startDate":0,
+            "endDate":0,
+            "Field of Study":0,
+            "Description":0
         }
         setEducation((prev)=>{
             return [...prev,newEducation]
         })
     }
 
+
+
     const [KeySkills,setKeySkills] = useState([])
 
     function addKeySkills(e){
+
         setKeySkills((prev)=>{
+            console.log(prev)
             return [...prev,`newvalue`]
         })
 
@@ -67,6 +73,16 @@ export default function Content(){
 
 
     return (
-        <div></div>
+        <div>
+            <UserDetails
+                FirstName={UserDetailes.FirstName}
+                LastName={UserDetailes.LastName}
+                Contact={UserDetailes.ContactNo}
+                Email={UserDetailes.Email}
+                Location={UserDetailes.Location}
+                Desc={UserDetailes.Description}
+                onchangeFn = {onChangeUserDetails}
+            />
+        </div>
     )
 }
---
+++ src/comps/UserDetails.jsx
@@ -0,0 +1,15 @@
+
+
+export default function UserDetails(props){
+
+    return(
+        <div id={"personalised_form"}>
+            <input type={"text"} value={props.FirstName} name={"FirstName"} onChange={props.onchangeFn}/>
+            <input type={"text"} value={props.LastName} name={"LastName"} onChange={props.onchangeFn}/>
+            <input type={"text"} value={props.Contact} name={"ContactNo"} onChange={props.onchangeFn}/>
+            <input type={"text"} value={props.Location} name={"Location"} onChange={props.onchangeFn}/>
+            <input type={"text"} value={props.Desc} name={"Description"} onChange={props.onchangeFn}/>
+        </div>
+
+    )
+}
